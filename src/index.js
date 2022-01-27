3b9f26a6e98ea36426c4ec43ebe4d3aa15b1369e
Elisey Cherkov <warriorofsparta252@gmail.com>
2022-01-27 03:00:42 +0200

    Add fonts. Add zeroing stylesheet. Add sprite file. Start working on header.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Test from './scripts/test';
 import json from './assets/json.json';
 import WezomLogo from './assets/images/wezom.png'
+import Logo from './assets/images/sprite.svg'
 import './styles/style.scss';
 
 const test = new Test('done', WezomLogo);
